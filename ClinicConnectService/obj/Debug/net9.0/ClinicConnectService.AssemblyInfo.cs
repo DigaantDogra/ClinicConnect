2bb71e8e26e63c52854b70ca3d3be7405af44841
Digaant Singh Dogra <128942073+DigaantDogra@users.noreply.github.com>
2025-03-16 14:18:58 -0400

    assembly update

--- ClinicConnectService/obj/Debug/net9.0/ClinicConnectService.AssemblyInfo.cs
+++ ClinicConnectService/obj/Debug/net9.0/ClinicConnectService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicConnectService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccb452c973b41d0aa28082694c1b751811469ebc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f499dd5ee835e7b96e512bc5aef92eaa5fd083a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicConnectService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicConnectService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
