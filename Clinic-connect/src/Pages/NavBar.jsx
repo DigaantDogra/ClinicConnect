52b2e56f6f96dd6fbaea0f2b0f72f357c0dd16d8
Digaant Singh Dogra <128942073+DigaantDogra@users.noreply.github.com>
2025-03-17 13:48:30 -0400

    navbar layout completed

--- Clinic-connect/src/Pages/NavBar.jsx
+++ Clinic-connect/src/Pages/NavBar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
     return <div className="fixed top-0 left-0 h-screen w-20 m-0 
                             flex flex-col
                             bg-white text-xl text-gray-600">
-                                
+        <div className="p-30" />
         <SideIcon icon={<BsHouseFill />} />
         <SideIcon icon={<BsFileEarmarkPerson />} />
         <SideIcon icon={<BsFileEarmarkArrowUpFill />} />
