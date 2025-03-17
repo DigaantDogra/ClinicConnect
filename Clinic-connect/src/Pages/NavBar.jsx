bffa20a3e8dc903b9ac2ea540979ab1678e4804a
Digaant Singh Dogra <128942073+DigaantDogra@users.noreply.github.com>
2025-03-17 15:51:04 -0400

    updated navbar

--- Clinic-connect/src/Pages/NavBar.jsx
+++ Clinic-connect/src/Pages/NavBar.jsx
@@ -8,12 +8,13 @@ export const Navbar = ({ section }) => {
         <div className="fixed top-0 left-0 h-screen w-20 m-0 
                                 flex flex-col
                                 bg-white text-xl text-gray-600">
-            <div className="p-30" />
-            <SideIcon icon={<BsHouseFill />} />
-            <SideIcon icon={<BsFileEarmarkPerson />} />
-            <SideIcon icon={<BsFileEarmarkArrowUpFill />} />
-            <SideIcon icon={<BsCalendar2CheckFill />} />
-            <SideIcon icon={<BsFileEarmarkTextFill />} />
+            <div className="max-h-4xl mt-40 py-4">
+                <SideIcon icon={<BsHouseFill />} />
+                <SideIcon icon={<BsFileEarmarkPerson />} />
+                <SideIcon icon={<BsFileEarmarkArrowUpFill />} />
+                <SideIcon icon={<BsCalendar2CheckFill />} />
+                <SideIcon icon={<BsFileEarmarkTextFill />} />
+            </div>
         </div>
         <section>
             {section}
