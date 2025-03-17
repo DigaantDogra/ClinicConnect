57251726b4520e9f9873e8dccf91567c030350eb
Digaant Singh Dogra <128942073+DigaantDogra@users.noreply.github.com>
2025-03-17 12:18:50 -0400

    Updated Navbar and index css

--- Clinic-connect/src/Pages/NavBar.jsx
+++ Clinic-connect/src/Pages/NavBar.jsx
@@ -1,15 +1,18 @@
-import { BsHouseFill } from "react-icons/bs"
+import { BsHouseFill, BsFileEarmarkPerson, BsFileEarmarkArrowUpFill, BsCalendar2CheckFill , BsFileEarmarkTextFill} from "react-icons/bs"
+
 
 export const Navbar = () => {
     
 
     return <div className="fixed top-0 left-0 h-screen w-20 m-0 
                             flex flex-col
-                            bg-white text-gray-600">
-        <SideIcon icon={<BsHouseFill />}></SideIcon>
-        <i>C</i>
-        <i>D</i>
-
+                            bg-white text-xl text-gray-600">
+                                
+        <SideIcon icon={<BsHouseFill />} />
+        <SideIcon icon={<BsFileEarmarkPerson />} />
+        <SideIcon icon={<BsFileEarmarkArrowUpFill />} />
+        <SideIcon icon={<BsCalendar2CheckFill />} />
+        <SideIcon icon={<BsFileEarmarkTextFill />} />
     </div>
 }
 
