5acc2ba15fe3a7e4078b7162d3e7ca33ecdd0b96
Digaant Singh Dogra <128942073+DigaantDogra@users.noreply.github.com>
2025-03-17 18:30:53 -0400

    Home section updated with buttons

--- Clinic-connect/src/Pages/Patient/Home/HomeSection.jsx
+++ Clinic-connect/src/Pages/Patient/Home/HomeSection.jsx
@@ -2,6 +2,24 @@
 
 export const HomeSection = () => {
     return <div className="bg-white h-200 w-445 rounded-2xl">
+        <div className="h-23 bg-gradient-to-r from-blue-200 to-yellow-100 rounded-t-2xl"/>
+        <div className="flex justify-center items-center mx-auto">
+            <button className="home-btn">
+                <h1 className="home-h1">Book Appointment</h1>
+            </button>
+
+            <button className="home-btn">
+                <h1 className="home-h1">Check Records</h1>
+            </button>
+
+            <button className="home-btn">
+                <h1 className="home-h1">Examine Health plan</h1>
+            </button>
+        </div>
+
+        <div>
+            
+        </div>
 
     </div>
 }
