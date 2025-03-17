849e7dbb0ef405bc2b19fb2ccde7dd29bfb94b4f
Arjun Anand <arjunanand@Arjuns-MacBook-Pro.local>
2025-03-17 19:33:54 -0400

    Made the patientrecord management page with both patient upload records and patient view records views as tab separated views

--- Clinic-connect/src/App.jsx
+++ Clinic-connect/src/App.jsx
@@ -1,16 +1,22 @@
-import './App.css'
-import { Navbar } from './Pages/NavBar'
+import './App.css';
+import { Navbar } from './Pages/NavBar';
+import  PatientRecordManagement  from './Pages/HealthcareMangement/Patient/PatientRecordManagement';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from "./Pages/Patient/Home/Home"
 
 function App() {
-
   return (
-    <>
-      <Navbar section={
-          <Home />
-        }/>
-    </>
-  )
+    <Router>
+      <Navbar />
+        <main className="flex-1 ml-20 p-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/patient/upload" element={<PatientRecordManagement />} />
+            
+          </Routes>
+        </main>
+    </Router>
+  );
 }
 
-export default App
+export default App;
