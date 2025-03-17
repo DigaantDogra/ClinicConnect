9772c1908822ac85db7f5cd78a506c2194bd76a5
Digaant Singh Dogra <128942073+DigaantDogra@users.noreply.github.com>
2025-03-17 15:45:44 -0400

    Changes done in navbar - created section in navbar

--- Clinic-connect/src/Pages/NavBar.jsx
+++ Clinic-connect/src/Pages/NavBar.jsx
@@ -1,19 +1,24 @@
 import { BsHouseFill, BsFileEarmarkPerson, BsFileEarmarkArrowUpFill, BsCalendar2CheckFill , BsFileEarmarkTextFill} from "react-icons/bs"
 
 
-export const Navbar = () => {
+export const Navbar = ({ section }) => {
     
 
-    return <div className="fixed top-0 left-0 h-screen w-20 m-0 
-                            flex flex-col
-                            bg-white text-xl text-gray-600">
-        <div className="p-30" />
-        <SideIcon icon={<BsHouseFill />} />
-        <SideIcon icon={<BsFileEarmarkPerson />} />
-        <SideIcon icon={<BsFileEarmarkArrowUpFill />} />
-        <SideIcon icon={<BsCalendar2CheckFill />} />
-        <SideIcon icon={<BsFileEarmarkTextFill />} />
-    </div>
+    return <>
+        <div className="fixed top-0 left-0 h-screen w-20 m-0 
+                                flex flex-col
+                                bg-white text-xl text-gray-600">
+            <div className="p-30" />
+            <SideIcon icon={<BsHouseFill />} />
+            <SideIcon icon={<BsFileEarmarkPerson />} />
+            <SideIcon icon={<BsFileEarmarkArrowUpFill />} />
+            <SideIcon icon={<BsCalendar2CheckFill />} />
+            <SideIcon icon={<BsFileEarmarkTextFill />} />
+        </div>
+        <section>
+            {section}
+        </section>
+    </>
 }
 
 const SideIcon = ({ icon }) => {
