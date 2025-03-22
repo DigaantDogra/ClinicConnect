d1b229239c71a086b1172ed968c6822824c86051
Digaant Singh Dogra <128942073+DigaantDogra@users.noreply.github.com>
2025-03-21 21:01:52 -0400

    main background canvas created

---
+++ Clinic-connect/src/Pages/BackgroundCanvas.jsx
@@ -0,0 +1,9 @@
+
+export const BackgroundCanvas = ({ section }) => {
+    return <>
+        <div className="bg-white h-200 w-445 rounded-2xl">
+        <div className="h-23 bg-gradient-to-r from-blue-200 to-yellow-100 rounded-t-2xl"/>
+            {section}
+        </div>
+    </>
+}
