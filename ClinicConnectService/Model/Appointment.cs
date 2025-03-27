bdfd4d71207cb9d056432a37edcc9443121060e7
Digaant Singh Dogra <128942073+DigaantDogra@users.noreply.github.com>
2025-03-27 16:34:28 -0400

    Updated User and Appointment

--- ClinicConnectService/Model/Appointment.cs
+++ ClinicConnectService/Model/Appointment.cs
@@ -2,4 +2,8 @@ namespace ClinicConnectService.Model;
 
 public class Appointment
 {
+    public required String Date { get; set; }
+    public required String Day { get; set; }
+    public required String Time { get; set; }
+    public required string Email { get; set; }
 }
--- ClinicConnectService/Model/User.cs
+++ ClinicConnectService/Model/User.cs
@@ -5,6 +5,6 @@ public class User
     public required string UserName { get; set; }
     public required string Email { get; set; }
     public UserType UserType { get; protected set; }
-    public required List<Appointment> Appointments { get; set; }
+    public List<Appointment>? Appointments { get; set; }
     public List<Notification>? Notifications { get; set; }
 }
